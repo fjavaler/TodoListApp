d22884a7867f48763527163e33afa453c37120c7
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-01 12:00:05 -0600

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Fred Javalera on 6/1/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
